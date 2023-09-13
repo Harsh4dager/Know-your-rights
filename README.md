2114dc634eaa65631f6e2bfd0f06d5e6e70ef82d
Harsh4dager <Harsh4dager>
2023-09-13 08:58:30 +0000

    Pushed from Locofy Builder

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-  # nyay jyoti_proto
+  # book landing page
 
   Note: Please ensure you have installed <code><a href="https://nodejs.org/en/download/">nodejs</a></code>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -40,3 +40,54 @@ module.exports = {
     preflight: false,
   },
 };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./!(build|dist|.*)/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "#fff",
+        darkslategray: "#013456",
+        gold: "#fad405",
+        black: "#000",
+        whitesmoke: {
+          "100": "#faf9f6",
+          "200": "#f4f1ec",
+        },
+        gainsboro: "#e5e4e3",
+        midnightblue: "#031968",
+        lightslategray: "#8796ba",
+        gray: "#fafafa",
+        orange: "#fea403",
+        steelblue: "#5b6792",
+      },
+      spacing: {},
+      fontFamily: {
+        poppins: "Poppins",
+        "istok-web": "'Istok Web'",
+        inter: "Inter",
+        "bebas-neue": "'Bebas Neue'",
+        "inria-sans": "'Inria Sans'",
+      },
+      borderRadius: {
+        "31xl": "50px",
+        "13xl": "32px",
+        "29xl": "48px",
+        "26xl": "45px",
+      },
+    },
+    fontSize: {
+      "13xl": "2rem",
+      xl: "1.25rem",
+      "17xl": "2.25rem",
+      base: "1rem",
+      "45xl": "4rem",
+      mini: "0.94rem",
+      "5xl": "1.5rem",
+      inherit: "inherit",
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+};
